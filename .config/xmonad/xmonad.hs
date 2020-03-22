295b9a2f0362a6f1e6dca077d4aa50b4dd364083
Victor Albertsson <victor.albertsson@live.se>
2020-03-22 23:50:34 +0100

    Added fontconfig dotfile and started the switch from dmenu to rofi.

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -5,13 +5,16 @@ where
 
 import           XMonad
 import           XMonad.Config.Desktop
+import           XMonad.Util.EZConfig
 
 main :: IO ()
 main = do
   spawn "xmobar"
-  xmonad $ desktopConfig { modMask            = mod4Mask
-                         , borderWidth        = 2
-                         , terminal           = "alacritty"
-                         , normalBorderColor  = "#cccccc"
-                         , focusedBorderColor = "#cd8b00"
-                         }
+  xmonad
+    $                desktopConfig { modMask            = mod4Mask
+                                   , borderWidth        = 2
+                                   , terminal           = "alacritty"
+                                   , normalBorderColor  = "#cccccc"
+                                   , focusedBorderColor = "#cd8b00"
+                                   }
+    `additionalKeys` [((mod4Mask, xK_p), spawn "rofi -show run")]
