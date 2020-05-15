23209ee46dfb02d7c17827aa069fccbe289ceaa4
Victor Albertsson <victor.albertsson@live.se>
2020-05-15 18:33:45 +0200

    Fixed the XMonad configuration

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -3,18 +3,23 @@ module Main
   )
 where
 
-import           XMonad
-import           XMonad.Config.Desktop
-import           XMonad.Util.EZConfig
+import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.EZConfig (additionalKeys)
+
+modkey = mod4Mask
 
 main :: IO ()
 main = do
-  spawn "xmobar"
-  xmonad
-    $                desktopConfig { modMask            = mod4Mask
-                                   , borderWidth        = 2
-                                   , terminal           = "alacritty"
-                                   , normalBorderColor  = "#cccccc"
-                                   , focusedBorderColor = "#cd8b00"
-                                   }
-    `additionalKeys` [((mod4Mask, xK_p), spawn "rofi -show run")]
+  xmproc <- spawnPipe "xmobar"
+  xmonad $ docks def
+    { modMask            = modkey
+    , borderWidth        = 2
+    , terminal           = "alacritty"
+    , normalBorderColor  = "#cccccc"
+    , focusedBorderColor = "#cd8b00"
+    , layoutHook         = avoidStruts $ layoutHook def
+    } `additionalKeys`
+    [ ((modkey, xK_p), spawn "rofi -show run")
+    ]
