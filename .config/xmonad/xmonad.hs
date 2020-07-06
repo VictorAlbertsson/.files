ef2fd9cb0f701c8d1cf67f09d6d72e502f41850c
Victor Albertsson <victor.albertsson@live.se>
2020-07-06 16:17:13 +0200

    Enabled proper EWMH functionality.

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -4,22 +4,34 @@ module Main
 where
 
 import XMonad
+import XMonad.Config.Desktop
+import XMonad.Layout.NoBorders
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
 
-modkey = mod4Mask
+superKey  = mod4Mask
 
 main :: IO ()
 main = do
   xmproc <- spawnPipe "xmobar"
-  xmonad $ docks def
-    { modMask            = modkey
+  xmonad $ ewmh $ docks def
+    { modMask            = superKey
     , borderWidth        = 2
     , terminal           = "alacritty"
     , normalBorderColor  = "#cccccc"
     , focusedBorderColor = "#cd8b00"
-    , layoutHook         = avoidStruts $ layoutHook def
+    , manageHook         = composeOne
+                           [ className =? "steam" -?> doFullFloat -- steam bigpicture mode
+                           , transience
+                           , isFullscreen -?> doFullFloat
+                           ]
+    , layoutHook         = avoidStruts $ smartBorders $ layoutHook def
+    , handleEventHook    = composeAll [ handleEventHook def
+                                      , fullscreenEventHook
+                                      ]
     } `additionalKeys`
-    [ ((modkey, xK_p), spawn "rofi -show run")
+    [ ((superKey, xK_p), spawn "rofi -show run")
     ]
