5d9a0c43f71d3447d598543f65471913acdafd5a
Victor Albertsson <victor.albertsson@live.se>
2020-11-29 19:53:37 +0100

    Refactored the terminal into its own variable

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -17,42 +17,9 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
 
-superMask  = mod4Mask
+myModMask  = mod4Mask
 
-main :: IO ()
-main = do
-  xmproc <- spawnPipe "xmobar"
-  xmonad . ewmh . docks $ def
-    { modMask            = superMask
-    , borderWidth        = 2
-    , terminal           = "alacritty"
-    , normalBorderColor  = "#cccccc"
-    , focusedBorderColor = "#cd8b00"
-    , manageHook         = composeOne
-                           [ className =? "steam" -?> doFullFloat -- steam bigpicture mode
-                           , transience
-                           , isFullscreen -?> doFullFloat
-                           ]
-    , layoutHook         = avoidStruts $ smartBorders $ layoutHook def
-    , handleEventHook    = composeAll
-                           [ handleEventHook def
-                           , fullscreenEventHook
-                           ]
-    , workspaces         = toWorkspaces myWorkspaces
-    } `additionalKeys`
-    [ ((superMask, xK_p), spawn "rofi -show run")
-    , ((superMask .|. shiftMask, xK_p), spawn "passmenu --type")
-    -- 'autoclicker' script at ~/.scripts/autoclicker
-    , ((superMask, xK_c), spawn "autoclicker")
-    -- Move focus to another workspace
-    , ((superMask, xK_f), treeselectWorkspace myTreeConf myWorkspaces greedyView)
-    -- Move both window and focus to another workspace
-    , ((superMask .|. shiftMask, xK_f), treeselectWorkspace myTreeConf myWorkspaces $ liftM2 (.) greedyView shift)
-    -- Open editor in a new tab (v for $VISUAL)
-    -- TODO replace nvim with $VISUAL
-    -- TODO replace alacritty with some terminal variable
-    , ((superMask, xK_v), spawn "alacritty -e nvim")
-    ]
+myTerminal = "alacritty"
 
 myTreeConf =
   TSConfig { ts_hidechildren = True
@@ -75,14 +42,48 @@ myWorkspaces =
     []
   , Node "Programming" 
     []
+  , Node "Writing" 
+    []
+  , Node "Socialization" 
+    []
   , Node "Launchers" 
     []
   , Node "Games" 
     []
   , Node "Web" 
     []
-  , Node "Drawing" 
-    []
-  , Node "Social Media" 
-    []
   ]
+
+main :: IO ()
+main = do
+  xmproc <- spawnPipe "xmobar"
+  xmonad . ewmh . docks $ def
+    { modMask            = myModMask
+    , borderWidth        = 2
+    , terminal           = myTerminal
+    , normalBorderColor  = "#cccccc"
+    , focusedBorderColor = "#cd8b00"
+    , manageHook         = composeOne
+                           [ className =? "steam" -?> doFullFloat -- steam bigpicture mode
+                           , transience
+                           , isFullscreen -?> doFullFloat
+                           ]
+    , layoutHook         = avoidStruts $ smartBorders $ layoutHook def
+    , handleEventHook    = composeAll
+                           [ handleEventHook def
+                           , fullscreenEventHook
+                           ]
+    , workspaces         = toWorkspaces myWorkspaces
+    } `additionalKeys`
+    [ ((myModMask, xK_p), spawn "rofi -show run")
+    , ((myModMask .|. shiftMask, xK_p), spawn "passmenu --type")
+    -- 'autoclicker' script at ~/.scripts/autoclicker
+    , ((myModMask, xK_c), spawn "autoclicker")
+    -- Move focus to another workspace
+    , ((myModMask, xK_f), treeselectWorkspace myTreeConf myWorkspaces greedyView)
+    -- Move both window and focus to another workspace
+    , ((myModMask .|. shiftMask, xK_f), treeselectWorkspace myTreeConf myWorkspaces $ liftM2 (.) greedyView shift)
+    -- Open editor in a new tab (v for $VISUAL)
+    -- TODO replace nvim with $VISUAL
+    , ((myModMask, xK_v), spawn $ myTerminal ++ " -e nvim")
+    ]
